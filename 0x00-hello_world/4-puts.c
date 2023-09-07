57c3e02f58f795ca9c2bfde7f3ea836a13438b9f
root <root@MrSuadey>
2023-09-07 20:37:01 +0300

    Updating 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,5 +6,6 @@ int main(void) /*This is the main function */
 {
 	/*Creation of puts function*/
 	puts("\"Programming is like building a multilingual puzzle");
+	/*This indicates the end of program*/
 	return (0);
 }

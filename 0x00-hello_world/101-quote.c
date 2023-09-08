c66638b2e85b38b57c557444cfe7fd5c7fb5a980
root <root@MrSuadey>
2023-09-08 06:11:08 +0300

    Creating Advance tasks files

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,10 @@
 #include <unistd.h>
-
+/**
+ * main - Entry Point
+ * Return: always 0 (success)
+ */
 int main() {
     const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
     write(2, message, sizeof(message) - 1);
-    return 1;
+    return (1);
 }

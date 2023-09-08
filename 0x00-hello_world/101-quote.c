d297916cbba95bbcfc4da3ce0fca20567a948cd2
root <root@MrSuadey>
2023-09-08 07:00:24 +0300

    Updating 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,8 @@
  * main - Entry Point
  * Return: always 0 (success)
  */
-int main() {
-    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, sizeof(message) - 1);
-    return (1);
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }

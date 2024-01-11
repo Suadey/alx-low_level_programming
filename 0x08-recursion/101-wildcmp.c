941d24f1c152f5dd3eef21279784512591e75cbf
Suadey <suadey1@gmail.com>
2024-01-11 12:54:09 +0100

    Update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,38 +1,25 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ *wildcmp - function that returns the power of number
+ *@s1: string
+ *@s2: string to compare
+ *Return: 1 if the strings are identical or 0 if it is not
  */
-int main(void)
+int wildcmp(char *s1, char *s2)
 {
-    int r;
 
-    r = wildcmp("main.c", "*.c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "m*a*i*n*.*c*");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "main.c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "m*c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "ma********************************c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "*");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "***");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "m.*c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "**.*c");
-    printf("%d\n", r);
-    r = wildcmp("main-main.c", "ma*in.c");
-    printf("%d\n", r);
-    r = wildcmp("main", "main*d");
-    printf("%d\n", r);
-    r = wildcmp("abc", "*b");
-    printf("%d\n", r);
-    return (0);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == '\0' && *(s2 + 1) != '\0' && *s2 == '*')
+		return (0);
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	}
+	return (0);
 }

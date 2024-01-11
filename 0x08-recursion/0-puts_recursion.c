ddc38b13704f93a3b688a01b69ba15bda70f79b8
Suadey <suadey1@gmail.com>
2024-01-11 12:11:19 +0100

    Adding 0-main.c file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,17 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _put_recursion - function name
+ * @s: parameter of the function
  */
-int main(void)
+
+void _puts_recursion(char *s)
 {
-	_puts_recursion("Puts with recursion");
-	return(0);
+	if(*s)
+	{
+		_putchar (*s);
+		_puts_recursion(s+1);
+	}
+	else
+	_putchar('\n');
 }

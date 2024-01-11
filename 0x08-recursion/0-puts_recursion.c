5070c69ab9e41c6dab2240c81645392aef96b0b7
Suadey <suadey1@gmail.com>
2024-01-11 12:17:48 +0100

    Update 0-puts_recursion file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,19 @@
 #include "main.h"
-
 /**
  * _put_recursion - function name
  * @s: parameter of the function
+ * Return: Always 0 (Success)
  */
 
 void _puts_recursion(char *s)
 {
-	if(*s)
+	if (*s)
 	{
 		_putchar (*s);
-		_puts_recursion(s+1);
+		_puts_recursion(s + 1);
 	}
 	else
-	_putchar('\n');
+	{
+		_putchar('\n');
+	}
 }
